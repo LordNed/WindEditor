3393525621cb5f031caa2d0e1e11f4d0980d0be4
Matt Hoffman <lord.matt.hoffman@gmail.com>
2014-06-01 15:18:52 -0700

    Added IEditorTool interface.

---
+++ WindViewer/src/Editor/Tools/IEditorTool.cs
@@ -0,0 +1,11 @@
+﻿namespace WindViewer.Editor
+{
+    public interface IEditorTool
+    {
+        void PreUpdate();
+        void Update();
+        void LateUpdate();
+
+        void Render();
+    }
+}
