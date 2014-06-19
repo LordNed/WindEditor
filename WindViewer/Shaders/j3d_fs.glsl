a7a05c9ec08b1974781a897530b695c6baa1ff6e
Matt Hoffman <lord.matt.hoffman@gmail.com>
2014-06-19 16:14:51 -0700

    Added a placeholder VS and FS for rendering J3D models.

---
+++ WindViewer/Shaders/j3d_fs.glsl
@@ -0,0 +1,16 @@
+﻿#version 330 core
+
+//Interpolated values from the vertex shaders
+in vec2 UV;
+
+//Output Data
+out vec3 color;
+
+//Texture Sampler
+uniform sampler2D diffuseTextureSampler;
+
+
+void main()
+{
+	color = texture(diffuseTextureSampler, UV).rgb;
+}
---
+++ WindViewer/Shaders/j3d_vs.glsl
@@ -0,0 +1,19 @@
+﻿#version 330 core
+
+//Input Data
+in vec3 vertexPos;
+in vec2 vertexUV;
+
+//output data
+out vec2 UV;
+
+//Values that stay constant for the whole mesh.
+uniform mat4 MVP;
+
+void main()
+{
+	//Output position of the vertex, in clip space : MVP * position
+	gl_Position = MVP * vec4(vertexPos, 1);
+
+	UV = vertexUV;
+}
